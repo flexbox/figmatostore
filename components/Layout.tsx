4b0a90c8126c4251a8062c2b4f4e3f6d4bc45281
David Leuliette <dleuliette@gmail.com>
2021-04-30 19:02:47 +0200

    add tailwindcss

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
+    <header className="bg-red-500">
       <nav>
         <Link href="/">
           <a>Home</a>
